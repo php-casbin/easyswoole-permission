4329b6829b77baadec1a666fb67e310c7ff972ad
basakest <ab1652759879@gmail.com>
2021-03-17 23:31:33 +0800

    fix: use 'Github Actions' to create casbin_rules table

--- tests/DatabaseAdapterTest.php
+++ tests/DatabaseAdapterTest.php
@@ -108,25 +108,4 @@ class DatabaseAdapterTest extends TestCase
         $adapter->savePolicy($model);
         $this->assertTrue($e->enforce('alice', 'data4', 'read'));
     }
-
-    public function testRemovePolicies()
-    {
-        $e = $this->getEnforcer();
-        $this->assertEquals([
-            ['alice', 'data1', 'read'],
-            ['bob', 'data2', 'write'],
-            ['data2_admin', 'data2', 'read'],
-            ['data2_admin', 'data2', 'write'],
-        ], $e->getPolicy());
-
-        $e->removePolicies([
-            ['data2_admin', 'data2', 'read'],
-            ['data2_admin', 'data2', 'write'],
-        ]);
-
-        $this->assertEquals([
-            ['alice', 'data1', 'read'],
-            ['bob', 'data2', 'write']
-        ], $e->getPolicy());
-    }
 }
