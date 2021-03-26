c03b263748d0a0ffa71ef9093f602c848c0ddb10
gaobinzhan <gaobinzhan@gmail.com>
2021-03-26 11:02:09 +0800

    refactor: A code change that neither fixes a bug nor adds a feature.

--- src/Config.php
+++ src/Config.php
@@ -4,112 +4,116 @@ declare(strict_types=1);
 
 namespace EasySwoole\Permission;
 
-use EasySwoole\Permission\Adapters\DatabaseAdapter;
+use Casbin\Persist\Adapter;
 
 class Config
 {
-  /**
-   * @var string
-   */
-  protected $model_config_type = 'file';
+    const CONFIG_TYPE_FILE = 'file';
 
-  /**
-   * @var string
-   */
-  protected $model_config_file_path = __DIR__ . '/casbin-rbac-model.conf';
+    const CONFIG_TYPE_TEXT = 'text';
 
-  /**
-   * @var string
-   */
-  protected $model_config_text = '';
+    /**
+     * @var string
+     */
+    protected $model_config_type = self::CONFIG_TYPE_FILE;
 
-  /**
-   * @var string
-   */
-  protected $adapter = DatabaseAdapter::class;
+    /**
+     * @var string
+     */
+    protected $model_config_file_path = __DIR__ . '/casbin-rbac-model.conf';
 
-  /**
-   * @var bool
-   */
-  protected $log_enable = false;
+    /**
+     * @var string
+     */
+    protected $model_config_text = '';
 
-  /**
-   * @return string
-   */
-  public function getModelConfigType(): string
-  {
-    return $this->model_config_type;
-  }
+    /**
+     * @var Adapter
+     */
+    protected $adapter;
 
-  /**
-   * @param string $model_config_type
-   */
-  public function setModelConfigType(string $model_config_type): void
-  {
-    $this->model_config_type = $model_config_type;
-  }
+    /**
+     * @var bool
+     */
+    protected $log_enable = false;
 
-  /**
-   * @return string
-   */
-  public function getModelConfigFilePath(): string
-  {
-    return $this->model_config_file_path;
-  }
+    /**
+     * @return string
+     */
+    public function getModelConfigType(): string
+    {
+        return $this->model_config_type;
+    }
 
-  /**
-   * @param string $model_config_file_path
-   */
-  public function setModelConfigFilePath(string $model_config_file_path): void
-  {
-    $this->model_config_file_path = $model_config_file_path;
-  }
+    /**
+     * @param string $model_config_type
+     */
+    public function setModelConfigType(string $model_config_type): void
+    {
+        $this->model_config_type = $model_config_type;
+    }
 
-  /**
-   * @return string
-   */
-  public function getModelConfigText(): string
-  {
-    return $this->model_config_text;
-  }
+    /**
+     * @return string
+     */
+    public function getModelConfigFilePath(): string
+    {
+        return $this->model_config_file_path;
+    }
 
-  /**
-   * @param string $model_config_text
-   */
-  public function setModelConfigText(string $model_config_text): void
-  {
-    $this->model_config_text = $model_config_text;
-  }
+    /**
+     * @param string $model_config_file_path
+     */
+    public function setModelConfigFilePath(string $model_config_file_path): void
+    {
+        $this->model_config_file_path = $model_config_file_path;
+    }
 
-  /**
-   * @return string
-   */
-  public function getAdapter(): string
-  {
-    return $this->adapter;
-  }
+    /**
+     * @return string
+     */
+    public function getModelConfigText(): string
+    {
+        return $this->model_config_text;
+    }
 
-  /**
-   * @param string $adapter
-   */
-  public function setAdapter(string $adapter): void
-  {
-    $this->adapter = $adapter;
-  }
+    /**
+     * @param string $model_config_text
+     */
+    public function setModelConfigText(string $model_config_text): void
+    {
+        $this->model_config_text = $model_config_text;
+    }
 
-  /**
-   * @return bool
-   */
-  public function isLogEnable(): bool
-  {
-    return $this->log_enable;
-  }
+    /**
+     * @return Adapter|null
+     */
+    public function getAdapter(): ?Adapter
+    {
+        return $this->adapter;
+    }
 
-  /**
-   * @param bool $log_enable
-   */
-  public function setLogEnable(bool $log_enable): void
-  {
-    $this->log_enable = $log_enable;
-  }
+    /**
+     * @param Adapter $adapter
+     */
+    public function setAdapter(Adapter $adapter): void
+    {
+        $this->adapter = $adapter;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isLogEnable(): bool
+    {
+        return $this->log_enable;
+    }
+
+    /**
+     * @param bool $log_enable
+     */
+    public function setLogEnable(bool $log_enable): void
+    {
+        $this->log_enable = $log_enable;
+    }
 }
